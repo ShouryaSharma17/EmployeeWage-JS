ef94a58b8c78614e32805ebadac1c30cb5b5c473
ShouryaSharma17 <shouryasharma17@gmail.com>
2021-05-19 21:46:28 +0530

    Added: Ability To Calculate Working Days Using Arrow Function

--- EmployeeWageProblem.js
+++ EmployeeWageProblem.js
@@ -216,7 +216,7 @@ const findTotal = (totalVal, dailyVal) =>
         });
     }
     console.log("Showing daily hours worked and wage using object "+empDailyWageAndHrArray);
-    //UC11 Created class for employee payroll
+    //UC11 Object using Arrow function
     {
         let totalWages = empDailyWageAndHrArray
                             .filter(dailyHrsAndWage => dailyHrsAndWage.dailyWage > 0) 
@@ -239,38 +239,3 @@ const findTotal = (totalVal, dailyVal) =>
     
             console.log("UC 11D NonWorkingDayNums: "+nonWorkingDayNums);
         }
-     //UC11(New)
-     {
-        // UC 11 class
-    
-        class EmployeePayrollData 
-        {
-            // property
-            id;
-            name; 
-            salary; 
-    
-           // constructor
-            constructor(id, name, salary) 
-           { 
-                this.id = id; 
-                this.name = name;
-                this.salary = salary;
-            } 
-    
-           // getter and setter method 
-           get name() { return this._name; } 
-           set name(name) { 
-           this._name = name; 
-           } 
-           
-           // method 
-           toString() {
-                return "id=" + this.id + ", name=" + this.name + ", salary=" + this.salary; 
-            } 
-        }
-            let employeePayrollData = new EmployeePayrollData(1, "Mark", 30000); 
-            console.log("UC-11 Class Defination "+employeePayrollData.toString());
-            employeePayrollData.name = "john"; 
-            console.log(employeePayrollData.toString()); 
-    }
